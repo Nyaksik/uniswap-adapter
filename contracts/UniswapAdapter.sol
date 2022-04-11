09c6114dd4e71d87095dd01023f4bac81727ef62
Nyaksik <makapori@gmail.com>
2022-04-11 15:19:45 +0700

    fix uniswapadapter. added tokenerc20

--- contracts/UniswapAdapter.sol
+++ contracts/UniswapAdapter.sol
@@ -9,9 +9,11 @@ import "@openzeppelin/contracts/interfaces/IERC20.sol";
 contract UniswapAdapter {
     using SafeERC20 for IERC20;
 
-    address public constant FACTORY =
+    address private constant FACTORY =
         0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
-    address public constant ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    address private constant ROUTER =
+        0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    address private constant WETH = 0xc778417E063141139Fce010982780140Aa0cD5Ab;
 
     function createPair(address tokenA, address tokenB)
         external
@@ -32,11 +34,7 @@ contract UniswapAdapter {
         address tokenA,
         address tokenB,
         uint256 amountADesired,
-        uint256 amountBDesired,
-        uint256 amountAMin,
-        uint256 amountBMin,
-        address to,
-        uint256 deadline
+        uint256 amountBDesired
     )
         external
         returns (
@@ -50,67 +48,62 @@ contract UniswapAdapter {
             address(this),
             amountADesired
         );
-        IERC20(tokenA).approve(ROUTER, amountADesired);
         IERC20(tokenB).safeTransferFrom(
             msg.sender,
             address(this),
             amountBDesired
         );
+        IERC20(tokenA).approve(ROUTER, amountADesired);
         IERC20(tokenB).approve(ROUTER, amountBDesired);
         (amountA, amountB, liquidity) = IUniswapV2Router02(ROUTER).addLiquidity(
             tokenA,
             tokenB,
             amountADesired,
             amountBDesired,
-            amountAMin,
-            amountBMin,
-            to,
-            deadline
+            1,
+            1,
+            address(this),
+            block.timestamp
         );
-        if (amountADesired > amountA)
-            IERC20(tokenA).safeTransfer(msg.sender, amountADesired - amountA);
-        if (amountBDesired > amountB)
-            IERC20(tokenB).safeTransfer(msg.sender, amountBDesired - amountA);
     }
 
-    function removeLiquidity(
-        address tokenA,
-        address tokenB,
-        uint256 liquidity,
-        uint256 amountAMin,
-        uint256 amountBMin,
-        address to,
-        uint256 deadline
-    ) external returns (uint256 amountA, uint256 amountB) {
+    function removeLiquidity(address tokenA, address tokenB)
+        external
+        returns (uint256 amountA, uint256 amountB)
+    {
         address pair = IUniswapV2Factory(FACTORY).getPair(tokenA, tokenB);
-        IERC20(pair).safeTransferFrom(msg.sender, address(this), liquidity);
+        uint256 liquidity = IERC20(pair).balanceOf(address(this));
         IERC20(pair).approve(ROUTER, liquidity);
         (amountA, amountB) = IUniswapV2Router02(ROUTER).removeLiquidity(
             tokenA,
             tokenB,
             liquidity,
-            amountAMin,
-            amountBMin,
-            to,
-            deadline
+            1,
+            1,
+            address(this),
+            block.timestamp
         );
     }
 
-    function swapTokensForExactTokens(
-        uint256 amountOut,
-        uint256 amountInMax,
-        address[] calldata path,
-        address to,
-        uint256 deadline
+    function swap(
+        address tokenIn,
+        address tokenOut,
+        uint256 amountIn,
+        uint256 amountOutMin
     ) external returns (uint256[] memory amounts) {
-        IERC20(path[0]).safeTransferFrom(msg.sender, address(this), amountOut);
-        IERC20(path[0]).approve(ROUTER, amountOut);
-        amounts = IUniswapV2Router02(ROUTER).swapTokensForExactTokens(
-            amountOut,
-            amountInMax,
+        IERC20(tokenIn).safeTransferFrom(msg.sender, address(this), amountIn);
+        IERC20(tokenIn).approve(ROUTER, amountIn);
+        address[] memory path;
+        path = new address[](3);
+        path[0] = tokenIn;
+        path[1] = WETH;
+        path[2] = tokenOut;
+        amounts = IUniswapV2Router02(ROUTER).swapExactTokensForTokens(
+            amountIn,
+            amountOutMin,
             path,
-            to,
-            deadline
+            msg.sender,
+            block.timestamp
         );
     }
 }
